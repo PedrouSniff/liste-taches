f5eade095ac500c7af57de86efaa9394975ca22e
Pedro De Almeida Rodrigues <pedrodealmeidarodrigues13@gmail.com>
2024-10-31 11:56:27 +0100

    pas totalement fini

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,24 @@
 const todo = document.getElementsByClassName("tâchesencours")[0];
 const erreur = document.getElementsByClassName("erreur")[0];
-let table =[""]
+const fini = document.getElementsByClassName("tâchesfini")[0];
+let table =[]
 
 document.getElementById("button").addEventListener("click", addtâche)
 
 function addtâche() {
 
-    const askuserinput = document.getElementById("userinput").value.trim().toLocaleUpperCase()
+    const askuserinput = document.getElementById("userinput").value.trim().toLocaleLowerCase()
     erreur.textContent = ("")
 
     if (askuserinput === "") {
         erreur.textContent = "Insérez du texte ";
     }
-    else if (table.includes(askuserinput) === true) {
+    else if (table.includes(askuserinput.toLocaleLowerCase()) === true) {
         erreur.textContent = ("Ce mot est déjà présent dans la liste ;)")
     }else {
         const tacheinput = document.createElement("div")
 
-        table.push(askuserinput)
+        table.push(askuserinput.toLocaleLowerCase())
 
         todo.appendChild(tacheinput)
         tacheinput.textContent = askuserinput
@@ -37,6 +38,19 @@ function addtâche() {
         btn2.classList.add("btn2")
         btn2.textContent = ("Supprimer")
 
-        
+        btn2.addEventListener('click', function()
+        {
+            todo.removeChild(tacheinput)
+            table.pop(askuserinput.value)
+        })
+
+        btn1.addEventListener('click', function()
+        {
+            fini.appendChild(tacheinput)
+            table.pop(askuserinput.value)
+        })
+
+
+
     }
 }
